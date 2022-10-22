1cfa34fc9d2984af56ef70a1c2de34e911225ec5
João Marcus <joao.marcus@a3data.com.br>
2022-10-22 11:54:00 -0300

    first commit

---
+++ src/main/java/br/com/fiap/avctechapi/AvctechapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.avctechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvctechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvctechapiApplication.class, args);
+	}
+
+}
